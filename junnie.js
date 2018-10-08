c67950560b406e479725e140237fafb7413ed7bf
Jonathan <jmganz@ucdavis.edu>
2018-10-08 13:03:47 -0700

    Modify the javascript to print a watermark

--- junnie.js
+++ junnie.js
@@ -1,2 +1,2 @@
-var header = document.getElementsByClassName("masthead");
-header.innerHTML += "Brought to you by junnie";
+var cf-app = document.getElementsByTagName("cloudflare-app")[0];
+cf-app.innerHTML += "Brought to you by junnie";
